5d885138f18dfb2cf67d072ce0238f4c61d02f3c
vonqo <lupino.liberty@gmail.com>
2020-07-30 12:10:44 +0800

    osx project setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# <img height="42px" src="https://github.com/punkowl/gestalt/blob/master/public/devola-logo.png" />
+
+
+[![Shitcoded][ulaanbaatar-badge]][ub-wiki]
+
+Devola is real-time music visualizing tool that able to combine multiple types of visualing effects.
+
+
+## Copyright & License
+Copyright (c) 2020 Enkh-Amar.G - Released under the [Mozilla Public License Version 2.0](LICENSE)
+
+[devola-logo]: https://github.com/lupino22/gestalt/blob/master/public/logo.png
+[codacy]: https://www.codacy.com/app/lupino22/gestalt?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=lupino22/gestalt&amp;utm_campaign=Badge_Grade
+[ulaanbaatar-badge]: https://img.shields.io/badge/shitcoded%20in-%F0%9F%87%B2%F0%9F%87%B3ulaanbaatar-brightgreen.svg
+[ub-wiki]: https://en.wikipedia.org/wiki/Ulaanbaatar
---
+++ devola-osx/src/ofApp.cpp
@@ -0,0 +1,81 @@
+#include "ofApp.h"
+
+ofPolyline line;
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0,0,0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    for (auto &vert : line.getVertices()){
+        vert.x += ofRandom(-1,1);
+        vert.y += ofRandom(-1,1);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    // ofEnableAlphaBlending();
+    // ofSetColor(30,30,30,30);
+    line.draw();
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    ofPoint pt;
+    pt.set(x,y);
+    line.addVertex(pt);
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    line.clear();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
